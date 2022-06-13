9f2284e1b5509315b9c65c904cf5c2b1e9464f1a
farabi1 <rashidfarabi@gmail.com>
2022-06-14 01:02:54 +0600

    Online Reservation BY Farabi,Taaha,Shihab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to Perform.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='text-center font-semibold text-2xl'>Online Reservation and booking BY Farabi ,Taaha , Shihab</h1>
     </div>
   );
 }
