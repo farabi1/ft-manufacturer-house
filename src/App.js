3c9d963231ebbe87cc8308735d4e3ecf2b81fa0f
farabi1 <rashidfarabi@gmail.com>
2022-06-14 01:33:28 +0600

    Day 1 - Project Wireframe setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-center font-semibold text-2xl'>Online Reservation and booking BY Farabi ,Taaha , Shihab</h1>
+      <h1 className='text-center font-semibold text-2xl'>Day 1 - Online Reservation and booking BY Farabi ,Taaha , Shihab</h1>
     </div>
   );
 }
